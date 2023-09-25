9934e53c62986af67cdea3002d54de203122c5fb
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-09-25 17:25:31 +0200

    no scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,7 @@
 @extends('layouts.public')
 
 
-
 @section('content')
-    <div class="my-hero-section py-5">
-        <div class="container">
-            <h3>CURRENT SERIES</h3>
-        </div>
-    </div>
-
     <div class="comics-container">
         <div class="container my-5">
             <div class="row gy-4 pt-3">
