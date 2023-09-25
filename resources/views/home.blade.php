deb4381e6dae72cd80db95cc6fbf4d4004cb14b6
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-09-25 19:39:27 +0200

    route done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,20 @@
     <div class="comics-container">
         <div class="container my-5">
             <div class="row gy-4 pt-3">
-                <div class="col-2">
-
-                </div>
+                @foreach($home as $comic)
+                    <div class="col-2">
+                        <div class="card">
+                            <a href="#0">
+                                <div class="img-container">
+                                    <img src="{{ $comic['thumb'] }}" class="card-img" alt="{{ $comic['title'] }}">
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $comic['series'] }}</h5>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
         <div class="container d-flex justify-content-center">
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'home' => config('comics')
+    ]);
 });
 
