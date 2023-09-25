10e5101695abf468336862e9f14604b8ced5d125
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-09-25 16:03:37 +0200

    add home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.public')
+
+
+
+@section('content')
+    <div class="my-hero-section py-5">
+        <div class="container">
+            <h3>CURRENT SERIES</h3>
+        </div>
+    </div>
+
+    <div class="comics-container">
+        <div class="container my-5">
+            <div class="row gy-4 pt-3">
+                <div class="col-2">
+
+                </div>
+            </div>
+        </div>
+        <div class="container d-flex justify-content-center">
+            <button class="btn btn-primary mb-3 px-5 rounded-0">LOAD MORE</button>
+        </div>
+    </div>
+@endsection
